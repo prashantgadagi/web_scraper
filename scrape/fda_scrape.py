fc3b08be56605f493ec9e5fa29861c0de07b1cac
Prashant Gadagi <prashantgadagi88@gmail.com>
2020-09-19 22:36:32 -0700

    Modifications

--- scrape/fda_scrape.py
+++ scrape/fda_scrape.py
@@ -14,27 +14,26 @@ with open(filename, 'w') as csv_file:
     # contains all the child pages
     part_list = []
     while option is not None:
-        val = int(option.attrs['value'])
-        if val != 0:
-            part_list.append(val)
+        key = int(option.attrs['value'])
+        if key != 0:
+            part_list.append(key)
         if len(option.contents) > 1:
             option = option.contents[1]
         else:
             option = None
 
     # Iterate for each post
-    for part in part_list:
-        data = {"CFRPart": part, "Search": "search"}
+    for key in part_list:
+        data = {"CFRPart": key, "Search": "search"}
         response = requests.post(fda_url, data)
         page_soup = Soup(response.text, "html.parser")
-        column1 = page_soup.table.tr.td.table.findAll("tr")[3].div.find("a").text
+        column1 = "\"" + page_soup.table.tr.td.table.findAll("tr")[3].div.find("a").text + "\""
         items = page_soup.table.tr.td.table.findAll("tr")[3].findAll("td")[0].findAll("strong")
         for item in items:
             if item.a is not None:
                 data = item.a['href'].split('?')[1]
                 response = requests.get(fda_url, data)
                 page_soup = Soup(response.text, "html.parser")
-                # all_rows = page_soup.table.tr.td.table.findAll("tr")
                 all_rows = list(page_soup.table.tr.td.table.children)
                 row_counter = 0
                 while row_counter < len(all_rows):
@@ -44,14 +43,15 @@ with open(filename, 'w') as csv_file:
                     if all_rows[row_counter].strong is not None:
                         break
                     row_counter += 1
-                column2 = all_rows[row_counter].strong.text.strip().split('--')[1]
+                column2 = "\"" + all_rows[row_counter].strong.text.strip().split('--')[1] + "\""
                 for row_num in range(row_counter, len(all_rows)):
                     row = all_rows[row_num]
                     if row is not None and row != '\n' and len(row.findAll("table")) >= 2:
                         table1 = row.findAll("table")[0]
                         text_arr = table1.tr.td.text.strip().split(' ')
-                        column3 = text_arr[1:][0]
-                        column4 = " ".join(text_arr[1:][1:])
-                        column5 = "\"" + " ".join(list(row.findAll("table")[1].strings)).strip().replace("\n", "") + "\""
+                        column3 = "\"" + text_arr[1:][0] + "\""
+                        column4 = "\"" + " ".join(text_arr[1:][1:]) + "\""
+                        cntnt = " ".join(list(row.findAll("table")[1].strings)).strip().replace("\n", "")
+                        column5 = "\"" + (cntnt[:32760] + '...') if len(cntnt) > 32767 else cntnt + "\""
                         fields = [column1, column2, column3, column4, column5]
                         csv_writer.writerow(fields)
